406052ced2f00bc2d83f01382849a2b314e6b071
Sanjailievska <sanjailievska10@gmail.com>
2025-03-08 22:45:41 +0100

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class RealCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter the First number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Operation (+, -, *, /): ");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+        bool validOperation = true;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.");
+                    validOperation = false;
+                }
+                break;
+            default:
+                Console.WriteLine("Error: Invalid operation.");
+                validOperation = false;
+                break;
+        }
+
+        if (validOperation)
+            Console.WriteLine("The result is: " + result);
+    }
+}
